0d2245155df9fb5c77f25aced3c2d761a190004b
Jenny Peers <jcmansley-peers@hotmail.com>
2022-08-16 13:03:12 +0100

    using setTimeouts to make a production line

--- index.js
+++ index.js
@@ -17,8 +17,30 @@ const order = (fruitsName, callProduction) => {
 const production = () => {
   setTimeout(() => {
     console.log("production has started");
+
     setTimeout(() => {
       console.log(`The fruit has been chopped`);
+
+      setTimeout(() => {
+        console.log(
+          `the ${stocks.liquid[0]} and ${stocks.liquid[1]} was added`
+        );
+        setTimeout(() => {
+          console.log("The machine has started to make the ice-cream!");
+
+          setTimeout(() => {
+            console.log(`The ${stocks.holder[1]} has been selected`);
+
+            setTimeout(() => {
+              console.log(`The ${stocks.toppings[0]} has been selected`);
+
+              setTimeout(() => {
+                console.log("Your ice-cream is ready! Come again, Thank you!");
+              }, 2000);
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
     }, 2000);
   }, 0000);
 };
